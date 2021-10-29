a13b4147d014dbf5b09f391bc79d58285ff6a2a5
Ruochen-Wang <rwang113@terpmail.umd.edu>
2021-10-29 17:54:51 -0400

    cleaned up; added test files

--- eq_hist_baseline.cpp
+++ eq_hist_baseline.cpp
@@ -19,34 +19,17 @@ void cal_lut(unsigned char *src, uint8_t *lut);
 uint8_t sat_cast(uint16_t scaled_number);
 
 int main(){
-//    string src_path = "./input.jpg";
-//    string dst_path = "./output.jpg"
-//    Mat src = imread( samples::findFile(src_path), IMREAD_COLOR );
-//    if( src.empty() )
-//    {
-//        cout << "Could not open or find the image!\n" << endl;
-//        return -1;
-//    }
-
-//    cvtColor( src, src, COLOR_BGR2GRAY);
-
-//    Mat dst;
-//    unsigned char *src = malloc(sizeof(unsigned char)*IMAGE_SIZE);
-
     // prepare input
     std::ifstream input_file("0a9da1fa077e_d510dfa4b13d.b", std::ifstream::binary);
     std::ofstream output_file("0a9da1fa077e_d510dfa4b13d_equalized.b", std::ios::out | std::ios::binary);
-//    std::ofstream src_file("src.txt", std::ios::out | std::ios::binary);
+
     unsigned char *src = new unsigned char[IMAGE_SIZE];
     input_file.read((char *)src, IMAGE_SIZE);
 
     unsigned char *dst = new unsigned char [IMAGE_SIZE];
-//    uint8_t *lut = new uint8_t[INTENSITY_SPACE];
-//    cal_lut(src, lut);
     eq_hist(src, dst);
 
     output_file.write((char *)dst, IMAGE_SIZE);
-//    src_file.write((char *)src, IMAGE_SIZE);
 
     return 0;
 }
@@ -81,22 +64,11 @@ float compare_hist(unsigned int *H1, unsigned int *H2){
 *   @output: lut: pointer to an array of INTENSITY_SPACE number of uint8_t
 */
 void cal_lut(unsigned char *src, uint8_t *lut){
-    
     int localHist[INTENSITY_SPACE] = {};
 
     // collect histogram
-//    FILE *hf = fopen("hist.txt", "w+");
     for (int i = 0; i < IMAGE_SIZE; i++)
         localHist[(unsigned char)src[i]]++;
-/*
-    int hist_sum = 0;
-    for(int i = 0; i < INTENSITY_SPACE; i++){
-        fprintf(hf, "%d: %d\n", i, localHist[i]);
-        hist_sum += localHist[i];
-    }
-    fprintf(hf, "sum check: %d %d\n", hist_sum, hist_sum==(512*512));
-    fclose(hf);
-*/
 
     // find the first non-zero intensity
     int i = 0;
