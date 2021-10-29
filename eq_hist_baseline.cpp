7b723b8647319d3df425b92c825c30001da24f45
Ruochen-Wang <rwang113@terpmail.umd.edu>
2021-10-28 22:19:13 -0400

    eliminated openCV; fixed syntax errors and bugs;

--- eq_hist_baseline.cpp
+++ eq_hist_baseline.cpp
@@ -2,53 +2,63 @@
 #include <stdint.h>
 #include <string>
 #include <math.h>
-#include "opencv2/imgcodecs.hpp"
+#include <fstream>
+//#include "opencv2/imgcodecs.hpp"
 
-using namespace cv;
-using namespace std;
+//using namespace cv;
+//using namespace std;
 
 #define WIDTH                   512
 #define HEIGHT                  512
-#define IMAGE_SIZE              WID*HGT
+#define IMAGE_SIZE              WIDTH*HEIGHT
 #define INTENSITY_SPACE         256
 
-void eq_hist(unsigned char *src, unsigned char *dst);
-void cal_lut(unsigned char *src, uint8_t *lut);
-void sat_cast(uint16_t scaled_number);
+void eq_hist(char *src, unsigned char *dst);
+void cal_lut(char *src, uint8_t *lut);
+uint8_t sat_cast(uint16_t scaled_number);
 
-void main(){
-    string src_path = "./input.jpg";
-    string dst_path = "./output.jpg"
-    Mat src = imread( samples::findFile(src_path), IMREAD_COLOR );
-    if( src.empty() )
-    {
-        cout << "Could not open or find the image!\n" << endl;
-        return -1;
-    }
+int main(){
+//    string src_path = "./input.jpg";
+//    string dst_path = "./output.jpg"
+//    Mat src = imread( samples::findFile(src_path), IMREAD_COLOR );
+//    if( src.empty() )
+//    {
+//        cout << "Could not open or find the image!\n" << endl;
+//        return -1;
+//    }
 
-    cvtColor( src, src, COLOR_BGR2GRAY);
+//    cvtColor( src, src, COLOR_BGR2GRAY);
 
-    Mat dst;
-    //unsigned char *dst = malloc(sizeof(unsigned char)*IMAGE_SIZE);
+//    Mat dst;
+//    unsigned char *src = malloc(sizeof(unsigned char)*IMAGE_SIZE);
+
+    // prepare input
+    std::ifstream input_file("0a9da1fa077e_d510dfa4b13d.b", std::ifstream::binary);
+    char *src = new char[IMAGE_SIZE];
+    input_file.read(src, IMAGE_SIZE);
+
+    unsigned char *dst = new unsigned char [IMAGE_SIZE];
+
     eq_hist(src, dst);
-    imwrite(dst_path, dst);
+//    imwrite(dst_path, dst);
 
     free(src);
     free(dst);
+    return 0;
 }
 
-void eq_hist(unsigned char *src, unsigned char *dst){
-    uint8_t *lut = malloc(sizeof(uint8_t)*INTENSITY_SPACE);
+void eq_hist(char *src, unsigned char *dst){
+    uint8_t *lut = new uint8_t[INTENSITY_SPACE];
     cal_lut(src, lut);
     
     for (int i = 0; i < IMAGE_SIZE; ++i) {
-        dst[i] = lut[src[i]];
+        dst[i] = lut[(unsigned char)src[i]];
     }
 
     free(lut);
 }
 
-void compare_hist(unsigned int *H1, unsigned int *H2){
+float compare_hist(unsigned int *H1, unsigned int *H2){
     // TODO: 
     //1. normalize H1 and H2 by dividing the IMAGE_SIZE.
     //2. calculate distance of two array H1 and H2. (L2 distance)
@@ -67,22 +77,20 @@ void compare_hist(unsigned int *H1, unsigned int *H2){
 *   @input: src: pointer to an array of IMAGE_SIZE number of unsigned char
 *   @output: lut: pointer to an array of INTENSITY_SPACE number of uint8_t
 */
-void cal_lut(unsigned char *src, uint8_t *lut){
-    int *localHist;
-    int total = IMAGE_SIZE;
-    int hist_sz = INTENSITY_SPACE;
-
-    localHist = calloc(sizeof(int), INTENSITY_SPACE);
+void cal_lut(char *src, uint8_t *lut){
+    int *localHist = new int [INTENSITY_SPACE];
+//    int total = IMAGE_SIZE;
+//    int hist_sz = INTENSITY_SPACE;
 
     // collect histogram
-    for (int i = 0; i < SZ; i)
+    for (int i = 0; i < INTENSITY_SPACE; i)
         localHist[src[i]]++;
 
     // find the first non-zero intensity
     int i = 0;
     while (!localHist[i]) ++i;
 
-    float scale = (hist_sz - 1.f)/(total - hist[i]);
+    float scale = (INTENSITY_SPACE - 1.f)/(IMAGE_SIZE - localHist[i]);
     int sum = 0;
 
     // find CDF
@@ -94,4 +102,4 @@ void cal_lut(unsigned char *src, uint8_t *lut){
     }
 }
 
-uint8_t sat_cast(uint16_t scaled_number){ return (uint8_t)std::min(scaled_number, 0xFFFF); }
+uint8_t sat_cast(uint16_t scaled_number){ return (uint8_t)std::min((uint16_t)scaled_number, (uint16_t)0xFFFF); }
